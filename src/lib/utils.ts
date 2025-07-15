b2c24224990c4dd2f82055368c8b3f1255b6fd09
Seereen Yangpaknam <seereen_2540@gmail.com>
2025-07-15 22:14:13 +0700

    add lib

---
+++ src/lib/utils.ts
@@ -0,0 +1,27 @@
+import { type ClassValue, clsx } from "clsx";
+
+export function cn(...inputs: ClassValue[]) {
+  return clsx(inputs);
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat("th-TH", {
+    style: "currency",
+    currency: "THB",
+  }).format(price);
+}
+
+export function formatDate(date: string): string {
+  return new Intl.DateTimeFormat("th-TH", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(new Date(date));
+}
+
+export function slugify(text: string): string {
+  return text
+    .toLowerCase()
+    .replace(/[^\w ]+/g, "")
+    .replace(/ +/g, "-");
+}
